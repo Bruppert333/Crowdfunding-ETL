25d73dbd0ec414f629af54e18436b824f66caa30
Bryce <bryceruppert@gmail.com>
2023-01-03 18:14:27 -0700

    upload

--- Starter_Code/crowdfunding_SQL_Analysis.sql
+++ Starter_Code/crowdfunding_SQL_Analysis.sql
@@ -1,21 +1,32 @@
 -- Challenge Bonus queries.
 -- 1. (2.5 pts)
 -- Retrieve all the number of backer_counts in descending order for each `cf_id` for the "live" campaigns. 
-
-
+select backers_count, cf_id, outcome
+from campaign
+where outcome = 'live'
+order by backers_count DESC;
 
 -- 2. (2.5 pts)
 -- Using the "backers" table confirm the results in the first query.
 
-
+select campaign.backers_count, campaign.cf_id, campaign.outcome, backers.first_name
+from campaign
+Inner join backers
+	on campaign.cf_id = backers.cf_id
+where outcome = 'live';
 
 -- 3. (5 pts)
 -- Create a table that has the first and last name, and email address of each contact.
 -- and the amount left to reach the goal for all "live" projects in descending order. 
+select goal - pledged as remaining_goal_amount, campaign.outcome, backers.first_name, backers.last_name, backers.email
+from campaign
+--into email_contacts_remaining_goal_amount
+Inner Join backers
+	on campaign.cf_id = backers.cf_id
+where outcome = 'live'
+Order by remaining_goal_amount DESC;
 
-
-
-
+select * from campaign;
 -- Check the table
 
 
